147cd1df8756eab16f80932f23b1eb6d4ee6213e
Gustavo Cotta <gmcotta34@gmail.com>
2021-03-06 15:56:04 -0300

    chore(lib): install eslint and husky

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,6 @@
-import { createGlobalStyle, ThemeProvider } from 'styled-components'
+import React from 'react';
+import { createGlobalStyle, ThemeProvider } from 'styled-components';
+import PropTypes from 'prop-types';
 
 const GlobalStyle = createGlobalStyle`
   body {
@@ -6,13 +8,13 @@ const GlobalStyle = createGlobalStyle`
     padding: 0;
     box-sizing: border-box;
   }
-`
+`;
 
 const theme = {
   colors: {
     primary: '#0070f3',
   },
-}
+};
 
 export default function App({ Component, pageProps }) {
   return (
@@ -22,5 +24,20 @@ export default function App({ Component, pageProps }) {
         <Component {...pageProps} />
       </ThemeProvider>
     </>
-  )
+  );
 }
+
+App.propTypes = {
+  Component: PropTypes.oneOfType([
+    PropTypes.func,
+    PropTypes.node,
+    PropTypes.elementType,
+    PropTypes.object,
+  ]).isRequired,
+  pageProps: PropTypes.oneOfType([
+    PropTypes.func,
+    PropTypes.node,
+    PropTypes.elementType,
+    PropTypes.object,
+  ]).isRequired,
+};
